876c85f74113aa63b8ac00adbed8bdcb15221b34
AnnaGreen1307 <annagreen1307@gmail.com>
2023-04-25 01:35:46 +0200

    Srednia wazona

--- avg.php
+++ avg.php
@@ -7,3 +7,25 @@ function srednia($tablica) {
     return 0;
   }
 }
+
+function weighted_average($values, $weights) {
+    // upewniamy się, że liczba wartości i wag jest taka sama
+    if(count($values) != count($weights)) {
+        return null;
+    }
+    
+    $sum = 0;
+    $weight_sum = 0;
+    for($i=0; $i<count($values); $i++) {
+        $sum += $values[$i] * $weights[$i];
+        $weight_sum += $weights[$i];
+    }
+    
+    // zabezpieczamy się przed dzieleniem przez zero
+    if($weight_sum == 0) {
+        return null;
+    }
+    
+    return $sum / $weight_sum;
+}
+
